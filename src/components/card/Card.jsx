4ae35df76c410e0238a0797f090bc198cfceec82
Xeni-ya <budyuginaxeniya@gmail.com>
2025-01-24 23:14:24 +0300

    feat: adding components

---
+++ src/components/card/Card.jsx
@@ -0,0 +1,8 @@
+function Card(props) {
+  return (
+    <div>
+    </div>
+  );
+}
+
+export default Card
---
+++ src/components/main/Main.jsx
@@ -0,0 +1,12 @@
+import Card from './components/card/Card';
+import React from 'react';
+
+function Main() {
+  return (
+    <React.Fragment>
+      <Card />
+    </React.Fragment>
+  );
+}
+
+export default Main
