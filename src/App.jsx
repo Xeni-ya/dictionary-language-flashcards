0134317cb5dee7e5f0d66612246ee4f59124d094
Xeni-ya <budyuginaxeniya@gmail.com>
2025-02-01 21:47:15 +0300

    fix: routing

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,23 @@ import Footer from './components/footer/Footer';
 import Home from './pages/home/Home';
 import Dictionary from './pages/dictionary/dictionary';
 import Card from './components/card/Card';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <main className="main">
-        <Home />
-        <Dictionary />
-        <Card />
-      </main>
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <main className="main">
+          <Routes>
+            <Route path="/" exact element={<Home />} />
+            <Route path="/cards" element={<Card />} />
+            <Route path="/dictionary" element={<Dictionary />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
