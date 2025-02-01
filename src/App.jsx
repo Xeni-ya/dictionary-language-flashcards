ae87061288b26840f9b402b0af9120df05435833
Xeni-ya <budyuginaxeniya@gmail.com>
2025-02-01 22:10:56 +0300

    feat: handling non-existent addresses (404 not found)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
 import Home from './pages/home/Home';
 import Dictionary from './pages/dictionary/dictionary';
 import Card from './components/card/Card';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Missing from './components/missing/Missing';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
             <Route path="/" exact element={<Home />} />
             <Route path="/cards" element={<Card />} />
             <Route path="/dictionary" element={<Dictionary />} />
+            <Route path="*" element={<Missing />} />
           </Routes>
         </main>
         <Footer />
---
+++ src/components/missing/Missing.jsx
@@ -0,0 +1,11 @@
+import styles from './missing.module.css';
+
+const Missing = () => {
+  return (
+    <main className={styles.main}>
+      <img src="src\assets\img\error-page.svg" alt="error" className={styles.error__img} />
+    </main>
+  );
+}
+
+export default Missing;
